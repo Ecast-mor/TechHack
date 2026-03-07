5a1058e77fa2135965df033dd4ed72964b748335
Ecast-mor <eden.castrejon@icloud.com>
2026-03-07 15:31:03 -0500

    class implementation

--- Game_Engine/Echo_Sign.py
+++ Game_Engine/Echo_Sign.py
@@ -6,6 +6,75 @@ pygame.init()
 
 clock = pygame.time.Clock()
 
+# class Player():
+#     def __init__(self):
+#         self.health = 100
+#         self.attack_power = 3
+#         self.speed = 5
+#         self.level = 0
+
+#         self.animations = {}
+#         self.animation = "Idle"
+#         self.current_frame = 0
+#         self.animationTimer = 0
+
+#         self.load_images()
+
+#         self.image = self.animations[self.animation][0]
+#         self.rect = self.image.get_rect(center=(wW//2, wH//2))
+#         self.flipped = False
+#         self.facingLeft = False
+
+#         self.slash = pygame.image.load("slash.png")
+#         self.slashRect = None
+
+#         self.attacking = False
+
+       
+#     def load_images(self):
+#         folder = "Knight_1"
+
+#         for subfolder in os.listdir(folder):
+#             subfolderPath = os.path.join(folder, subfolder)
+
+#             if os.path.isdir(subfolderPath):
+#                 sprite_frames = []
+#                 for file in sorted(os.listdir(subfolderPath)):
+#                     full_path = os.path.join(subfolderPath, file)
+#                     image = pygame.image.load(full_path).convert_alpha()
+#                     sprite_frames.append(image)
+
+#                 self.animations[subfolder] = sprite_frames
+        
+        
+
+#     def animate(self, dt):
+#         frameSpeed = 100
+#         frames = self.animations[self.animation]
+
+#         self.animationTimer += dt
+
+#         if self.animationTimer >= frameSpeed:
+#             self.animationTimer = 0
+#             self.current_frame = (self.current_frame + 1) % len(frames)
+#             self.image = frames[self.current_frame]
+        
+#         if self.animation.startswith("Attack"):
+#             self.attacking = False
+#             self.slashRect = None
+        
+#     def flip_animation(self):
+#              for animation_name in self.animations:
+#                 flipped_frames = []
+#                 for frame in self.animations[animation_name]:
+#                     flipped = pygame.transform.flip(frame, True, False)
+#                     flipped_frames.append(flipped)
+
+#                 self.animations[animation_name] = flipped_frames
+
+#     def draw(self):
+#         screen.blit(self.image, self.rect)
+
 
 currentWindow = pygame.display.Info()
 windowHeight, windowWidth = currentWindow.current_h, currentWindow.current_w
@@ -32,15 +101,6 @@ def letter_choice():
     handImage = pygame.transform.scale(handImage, (100,100))
     handRect = handImage.get_rect(center =(windowWidth//2,windowHeight//2))
     screen.blit(handImage,handRect)
-
-
-    print(file)
-        
-
-start_time = pygame.time.get_ticks()
-duration = 3000 
-
-
 
 running = True
 while running:
