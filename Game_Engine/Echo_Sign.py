1b3d658f409178aeb0b69ea91f31c9f5eebcd096
Ecast-mor <eden.castrejon@icloud.com>
2026-03-06 21:50:11 -0500

    background image

--- Game_Engine/Echo_Sign.py
+++ Game_Engine/Echo_Sign.py
@@ -1,9 +1,18 @@
 import pygame
+import os
 
 pygame.init()
 currentWindow = pygame.display.Info()
 windowHeight, windowWidth = currentWindow.current_h, currentWindow.current_w
-screen = pygame.display.set_mode((windowWidth, windowHeight-60))
+screen = pygame.display.set_mode((windowWidth, windowHeight))
+
+background = "Game_Engine/tavern_background.png"
+
+
+image = pygame.image.load(background)
+image = pygame.transform.scale(image, (windowWidth,windowHeight))
+rect = image.get_rect(center=(windowWidth//2, windowHeight//2))
+
 
 
 running = True
@@ -11,5 +20,6 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    screen.blit(image,rect)
     pygame.display.update()
 pygame.quit
