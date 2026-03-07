5f3402b6e81905c42b783c7f3e09a2e590700151
Ecast-mor <eden.castrejon@icloud.com>
2026-03-07 14:43:14 -0500

    letter choice function

--- Game_Engine/Echo_Sign.py
+++ Game_Engine/Echo_Sign.py
@@ -1,6 +1,12 @@
 import pygame
+import random
+import os
 
 pygame.init()
+
+clock = pygame.time.Clock()
+
+
 currentWindow = pygame.display.Info()
 windowHeight, windowWidth = currentWindow.current_h, currentWindow.current_w
 screen = pygame.display.set_mode((windowWidth, windowHeight-60), pygame.SCALED)
@@ -16,15 +22,24 @@ sImage = pygame.image.load(scroll)
 sImage = pygame.transform.scale(sImage,(windowWidth*.30,windowHeight*.50))
 sRect = sImage.get_rect(midright=(windowWidth,windowHeight-windowHeight*.25))
 
-letters = "Game_Engine/transparent_letters.png"
-lImage = pygame.image.load(letters)
-lImage = pygame.transform.scale(lImage,(windowWidth*.20,windowHeight*.30))
-lRect = lImage.get_rect(center = (windowWidth-sRect.x,windowHeight-windowHeight*.25))
+def letter_choice():
+    folder = "Game_Engine/letters"
 
-class Game():
-    def init(self):
-        self.paused = False
-        self.start = False
+    folder_list = os.listdir(folder)
+    file = random.choice(folder_list)
+    full_path =  os.path.join(folder,file)
+    handImage = pygame.image.load(f"{full_path}")
+    handImage = pygame.transform.scale(handImage, (100,100))
+    handRect = handImage.get_rect(center =(windowWidth//2,windowHeight//2))
+    screen.blit(handImage,handRect)
+
+
+    print(file)
+        
+
+start_time = pygame.time.get_ticks()
+duration = 3000 
+
 
 
 running = True
@@ -32,11 +47,12 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
     
     screen.fill((0, 0, 0))
     screen.blit(image, rect)
     screen.blit(sImage,sRect)
-    screen.blit(lImage, lRect)
+    letter_choice()
     pygame.display.update()
 
 pygame.quit()
